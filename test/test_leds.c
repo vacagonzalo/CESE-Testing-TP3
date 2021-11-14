513e512e2b65bc85e61d6f926644e7cd772595ff
Gonzalo Nahuel Vaca <vacagonzalo@gmail.com>
2021-11-13 21:59:09 -0300

    no magic numbers

--- test/test_leds.c
+++ test/test_leds.c
@@ -1,6 +1,8 @@
 #include "unity.h"
 #include "leds.h"
 
+#define ALL_LEDS_ON 0xFFFF
+#define ALL_LEDS_OFF 0x0000
 #define NUMBER_OF_LEDS 16
 #define OFFSET_INDEX 1
 #define LED_BIT(X) (1 << (X - 1))
@@ -10,9 +12,9 @@ void tearDown() {}
 
 void test_system_starts_with_all_leds_off(void)
 {
-    uint16_t virtual_port = 0xFFFF;
+    uint16_t virtual_port = ALL_LEDS_ON;
     leds_create(&virtual_port);
-    TEST_ASSERT_EQUAL_HEX16(0x0000, virtual_port);
+    TEST_ASSERT_EQUAL_HEX16(ALL_LEDS_OFF, virtual_port);
 }
 
 void test_turn_on_one_led(void)
@@ -36,7 +38,7 @@ void test_turn_off_one_led(void)
          ++test_case)
     {
         leds_create(&virtual_port);
-        virtual_port = 0xFFFF;
+        virtual_port = ALL_LEDS_ON;
         leds_turn_off(&virtual_port, test_case);
         TEST_ASSERT_EQUAL_HEX16(~LED_BIT(test_case), virtual_port);
     }
@@ -52,6 +54,6 @@ void test_turn_on_and_off_one_led(void)
         leds_create(&virtual_port);
         leds_turn_on(&virtual_port, test_case);
         leds_turn_off(&virtual_port, test_case);
-        TEST_ASSERT_EQUAL_HEX16(0x0000, virtual_port);
+        TEST_ASSERT_EQUAL_HEX16(ALL_LEDS_OFF, virtual_port);
     }
 }
